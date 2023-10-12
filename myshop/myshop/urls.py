86a5146a00260038ed3ca1d4bf89331ff894f6c9
musakovskiy <magazynuniversam@gmail.com>
2023-10-12 16:25:18 +0300

    create cart

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart', include('cart.urls', namespace='cart')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
