095fbe414ac386e81aed620e0017245f66336ed5
musakovskiy <magazynuniversam@gmail.com>
2023-10-13 10:25:39 +0300

    final

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,16 +1,37 @@
+from decimal import Decimal
+from django.core.validators import MinValueValidator, \
+                                   MaxValueValidator
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from shop.models import Product
+from coupons.models import Coupon
+
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    first_name = models.CharField(_('first name'),
+                                  max_length=50)
+    last_name = models.CharField(_('last name'),
+                                 max_length=50)
+    email = models.EmailField(_('e-mail'))
+    address = models.CharField(_('address'),
+                               max_length=250)
+    postal_code = models.CharField(_('postal code'),
+                                   max_length=20)
+    city = models.CharField(_('city'),
+                            max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                       MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
@@ -21,8 +42,31 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
 
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            # no payment associated
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            # Stripe path for test payments
+            path = '/test/'
+        else:
+            # Stripe path for real payments
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
